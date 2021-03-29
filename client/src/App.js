b72826bc72e3e00cbacc5d29b21ad49a2ef7731f
Zach Taylor <ztayut2tx@gmail.com>
2021-03-29 15:30:31 -0500

    coded out Nav component

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import {BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Search from "./pages/search";
 import Saved from "./pages/saved";
+import Nav from "./components/Nav";
 
 function App() {
   return(
     <Router>
       <div>
+        <Nav />
         <Switch>
-          <Route exact path="/search">
+          <Route exact path="/">
             <Search />
           </Route>
           <Route exact path="/saved">
---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function Nav() {
+    return (
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="/">Google Books</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/">Search Books</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/saved">Saved Books</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+);
+}
+
+export default Nav;
+    )
+}
