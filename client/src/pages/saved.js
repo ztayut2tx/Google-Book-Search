bae66d7f7a2dbbc39110afc3d109cbefc78adf33
Zach Taylor <ztayut2tx@gmail.com>
2021-03-29 15:48:05 -0500

    component should generate on save page now

--- client/src/pages/saved.js
+++ client/src/pages/saved.js
@@ -1,8 +1,11 @@
 import React from "react";
+import SavedBooks from "../components/SavedBooks";
 
 function Saved() {
     return(
-        <div></div>
+        <div>
+            <SavedBooks />
+        </div>
     );
 }
 
