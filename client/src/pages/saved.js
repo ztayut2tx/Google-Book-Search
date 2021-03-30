d56a2bdf3e6859cd709a7d25ba1516d3200ec62d
Zach Taylor <ztayut2tx@gmail.com>
2021-03-30 15:47:06 -0500

    reused jumbotron component in saved.js and edited necassary change points. created function to save searched books, a function to populate the saved page with your saved books from the api, need to create a delete BTN component

--- client/src/pages/saved.js
+++ client/src/pages/saved.js
@@ -1,14 +1,48 @@
 import React, { useEffect, useState } from "react";
 import SavedBooks from "../components/SavedBooks";
+import Jumbotron from "../utils/Jumbotron";
+import Api from "../utils/Api";
 
-function saveBook() {
+function Saved() {
 
     const [books, setBooks] =useState([])
+
+    useEffect(() => { popBooks()}, [])
+
+    function popBooks () {
+        Api.getBook().then(res => setBooks(res.data)).catch(err => console.log(err))
+    };
+
+    function deleteBook(id) {
+        Api.deleteBook(id).then(res => popBooks()).catch(err => console.log(err))
+    };
 
     
     return(
         <div>
-            <SavedBooks />
+            <p className="lead">Your Saved Books</p>
+                <Jumbotron>
+                    {books.length ? (
+                        <ul className="list-group">
+                            {books.map(book => (
+                                <li className="list-group-item" key={book.id}>
+                                    
+                                    <p className="lead d-inline">{book.title} written by {book.authors}</p>
+                                    
+                                    <DeleteBtn onClick={() => deleteBook(book.id,book.title, book.description, book.thumbnail, book.previewLink)}  />
+                                    
+                                    <a className="btn btn-success mr-1" style={{ float: "right" }} href={book.infoLink} target="_blank" rel="noreferrer">View</a>
+                                    
+                                    <img src={book.img} className="img-thumbnail float-left mr-3" alt="oldBook"></img>
+                                    
+                                    <p className="lead">{book.description}</p>
+                                </li>
+                            ))}
+                        </ul>
+                    ) : (
+                        <h3>No saved Books</h3>
+                    )}   
+                </Jumbotron>
         </div>
     );
 }
